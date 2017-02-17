a4a3c0111e0b292e23d0b8390325f45aaa40e370
Vincent Potvin <vincent_1222@hotmail.com>
2017-02-16 19:18:05 -0500

    change

--- assignment01/js/script.js
+++ assignment01/js/script.js
@@ -13,25 +13,43 @@ $(document).ready(function() {
     
     $('#trois, #quatre').click(function () {
         $(this).animate({ height: "300px"}, 200 );
-    });
-    
-    $('.un').click(function () {
-        if ($('.trois, .quatre').height() == 300) {
-        $(this).animate({ height: "150px", width: "150px"}, 200 );
-        }    
-    });
+    });    
     
     
-    
-    $('.un').click(function () {
+    $('#un').click(function () {
         
-        if (    ($('#trois').height() == 300) &&
+        if  (
+                ($('#trois').height() == 300) &&
                 ($('#quatre').height() == 300) &&
-                ($('#cinq').height() == 300) &&
-                ($('#six').height() == 300) ){
+                ($('#cinq').width() == 300) &&
+                ($('#six').width() == 300)
+            ){
+                $(this).animate({ height:"150px", width:"150px"}, 200 );
+                $('#trois').animate({ width:"25px", height:"425px", top:"0px"}, 200 );
+                $('#quatre').animate({ width:"25px", height:"425px", bottom:"0px"}, 200 );
+                $('#cinq').animate({ height:"25px", width:"425px", right:"0px"}, 200 );
+                $('#six').animate({ height:"25px", width:"425px", left:"0px"}, 200 );
+                $('#deux').animate({ height:"450px", width:"450px"}, 200 );
+            }   
+   
+    });
+    
+    
+    $('#deux').click(function () {
         
-        $(this).animate({ height: "150px", width: "150px"}, 200 );
-        }    
+        if  (
+                ($('#trois').height() == 300) &&
+                ($('#quatre').height() == 300) &&
+                ($('#cinq').width() == 300) &&
+                ($('#six').width() == 300)
+            ){
+                $(this).animate({ height:"150px", width:"150px"}, 200 );
+                $('#trois').animate({ width:"25px", height:"425px", top:"0px"}, 200 );
+                $('#quatre').animate({ width:"25px", height:"425px", bottom:"0px"}, 200 );
+                $('#cinq').animate({ height:"25px", width:"425px", right:"0px"}, 200 );
+                $('#six').animate({ height:"25px", width:"425px", left:"0px"}, 200 );
+                $('#deux').animate({ height:"450px", width:"450px"}, 200 );
+            }   
    
     });
     
