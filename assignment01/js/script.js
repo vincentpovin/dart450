78de9b90dd7ea3bdfbee65f7787df392a5dbd074
Vincent Potvin <vincent_1222@hotmail.com>
2017-02-19 15:25:47 -0500

    chage

--- assignment01/js/script.js
+++ assignment01/js/script.js
@@ -1,5 +1,5 @@
 
-
+var temps = 300;
 
 
 $(document).ready(function() {
@@ -11,7 +11,7 @@ $(document).ready(function() {
                 ($('#deux').width() == 150)
             ){
         
-                $(this).animate({ width:"300px"}, 200 );
+                $(this).animate({ width:"300px"}, temps );
             }
     });
     
@@ -23,7 +23,7 @@ $(document).ready(function() {
                 ($('#deux').width() == 150)
             ){
         
-                $(this).animate({ height:"300px"}, 200 );
+                $(this).animate({ height:"300px"}, temps );
             }
     });    
     
@@ -37,12 +37,12 @@ $(document).ready(function() {
                 ($('#cinq').width() == 300) &&
                 ($('#six').width() == 300)
             ){
-                $(this).animate({ height:"150px", width:"150px"}, 200 );
-                $('#trois').animate({ width:"25px", height:"425px", top:"0px"}, 200 );
-                $('#quatre').animate({ width:"25px", height:"425px", bottom:"0px"}, 200 );
-                $('#cinq').animate({ height:"25px", width:"425px", right:"0px"}, 200 );
-                $('#six').animate({ height:"25px", width:"425px", left:"0px"}, 200 );
-                $('#deux').animate({ height:"450px", width:"450px"}, 200 );
+                $(this).animate({ height:"150px", width:"150px"}, temps );
+                $('#trois').animate({ width:"25px", height:"425px", top:"0px"}, temps );
+                $('#quatre').animate({ width:"25px", height:"425px", bottom:"0px"}, temps );
+                $('#cinq').animate({ height:"25px", width:"425px", right:"0px"}, temps );
+                $('#six').animate({ height:"25px", width:"425px", left:"0px"}, temps );
+                $('#deux').animate({ height:"450px", width:"450px"}, temps );
             }   
    
     });
@@ -58,9 +58,9 @@ $(document).ready(function() {
                 ($('#un').height() == 150)
 
             ){
-                $('#trois, #quatre').animate({ height:"0px"}, 200 );
-                $('#cinq, #six').animate({ width:"0px"}, 200 );
-                $('#un').animate({ width:"425px", height:"425px"}, 200 );
+                $('#trois, #quatre').animate({ height:"0px"}, temps );
+                $('#cinq, #six').animate({ width:"0px"}, temps );
+                $('#un').animate({ width:"425px", height:"425px"}, temps );
             }   
    
     });
@@ -74,9 +74,9 @@ $(document).ready(function() {
                 ($(this).height() == 425)
             ){
              
-                $('#un').animate({ width:"300px", height:"300px"}, 200 ); 
+                $('#un').animate({ width:"300px", height:"300px"}, temps ); 
             
-                $('#deux').animate({ backgroundColor:"white"}, 200 );   
+                $('#deux').animate({ backgroundColor:"white"}, temps );   
 
 
                 for (var i = 0; i < 9; i++) {
@@ -98,10 +98,11 @@ $(document).ready(function() {
         if  (
                 ($(this).width() == 300)
             ){                
+                $('#grand').animate({ backgroundColor:"#666666"}, temps );   
                 $('.r').fadeOut(200, function(){ $('.r').remove();});
-                $('#trois, #quatre').animate({ height:"225px", width:"225px"}, 200 );
-                $('#cinq, #six').animate({ width:"225px", height:"225px"}, 200, function() {
-                    $('#deux').animate({ backgroundColor:"black"}, 200 ); 
+                $('#trois, #quatre').animate({ height:"225px", width:"225px"}, temps );
+                $('#cinq, #six').animate({ width:"225px", height:"225px"}, temps, function() {
+                    $('#deux').animate({ backgroundColor:"black"}, temps ); 
                     });
 
                 }
@@ -120,13 +121,11 @@ $(document).ready(function() {
                 ($('#cinq').height() == 225) &&
                 ($('#six').height() == 225)
             ){                
-                $('#trois, #quatre').animate({ height:"0px"}, 200, function() {
-                    $('#cinq, #six').animate({ width:"0px"}, 200, function() {
-                        $('#un').animate({ width:"0px", height: "0px"}, 400);
-                        $('body').append('<div id="rond"></div>');
-                        $('#deux').animate({ width:"0px", height: "0px"}, 400 );
-                        $('#grand').animate({ width:"0px", height: "0px"}, 400, function() {
-                            $('#rond').animate({ width:"50px", height: "50px"}, 100);
+                $('#trois, #quatre').animate({ height:"0px"}, temps, function() {
+                    $('#cinq, #six').animate({ width:"0px"}, temps, function() {
+                        $('#un').animate({ width:"0px", height: "0px"}, temps);
+                        $('#deux').animate({ width:"0px", height: "0px"}, temps, function() {
+                            $('#rond').animate({ width:"75px", height: "75px"}, temps);
                         });
                     });
                 });
@@ -134,15 +133,87 @@ $(document).ready(function() {
     });
     
     
-   $('body').on('click', '#rond', function() {
-       if  (
-                ($(this).width() == 50)
-            ){
-
-                $(this).animate({ width:"150px", height: "150px"}, 200 );
-                $('body').append('<div id="rond2"></div>');
+    
+    
+    $('#rond').click(function () {
+        
+        if  (
+                ($(this).height() == 75)  
+            ){                 
+                $('#rond').animate({ height:"150px", width:"150px", backgroundColor:"#black" }, temps);    
+                $('#rond2').animate({ height:"75px", width:"75px" }, temps, function() {
+                    
+                    $('#rond').animate({ height:"225px", width:"225px", backgroundColor:"#FAFAD2"}, temps);
+                    $('#rond2').animate({ height:"150px", width:"150px", backgroundColor:"black" }, temps);
+                    $('#rond3').animate({ width:"75px", height: "75px"}, 200, function() {
+                        
+                        $('#rond').animate({ height:"300px", width:"300px", backgroundColor:"#90EE90"}, temps);
+                        $('#rond2').animate({ height:"225px", width:"225px", backgroundColor:"#FAFAD2"}, temps);
+                        $('#rond3').animate({ width:"150px", height: "150px", backgroundColor:"black"}, temps);
+                        $('#rond4').animate({ width:"75px", height: "75px"}, temps, function() {
+                            
+                            $('#rond').animate({ height:"375px", width:"375px", backgroundColor:"#87CEFA"}, temps);
+                            $('#rond2').animate({ height:"300px", width:"300px", backgroundColor:"#90EE90"}, temps);
+                            $('#rond3').animate({ width:"225px", height: "225px", backgroundColor:"#FAFAD2"}, temps);
+                            $('#rond4').animate({ width:"150px", height: "150px", backgroundColor:"black"}, temps);
+                            $('#rond5').animate({ width:"75px", height: "75px"}, temps, function() {
+                            
+                                $('#rond').animate({ height:"450px", width:"450px", backgroundColor:"#FFB6C1"}, temps);
+                                $('#rond2').animate({ height:"375px", width:"375px", backgroundColor:"#87CEFA"}, temps);
+                                $('#rond3').animate({ width:"300px", height: "300px", backgroundColor:"#90EE90"}, temps);
+                                $('#rond4').animate({ width:"225px", height: "225px", backgroundColor:"#FAFAD2"}, temps);
+                                $('#rond5').animate({ width:"150px", height: "150px", backgroundColor:"black"}, temps);
+                                $('#rond6').animate({ width:"75px", height: "75px"}, temps);
+                                
+                            });
+                        });
+                    });
+                });
             }
-});
+    });
+    
+    
+    
+    
+    $('#rond').click(function () {
+        
+        if  (
+                ($(this).height() == 450)  
+            ){                 
+                $('#rond').animate({backgroundColor:"black"}, temps);
+            }
+    });
+    
+    
+    
+    
+    $('#rond2').click(function () {
+        
+        if  (
+                ($(this).height() == 375)  
+            ){                 
+                $('#rond2').animate({backgroundColor:"white"}, temps);
+            }
+    });
+    
+    
+    
+    $('#rond3').click(function () {
+        
+        if  (
+                ($(this).height() == 375)  
+            ){                 
+                $('#rond2').animate({backgroundColor:"black"}, temps);
+            }
+    });
+    
+    
+    
+    
+    
+    
+    
+   
             
     
 
