805ad1d3321cd2e9da0f614cb6a39f10762ef721
Vincent Potvin <vincent_1222@hotmail.com>
2017-04-05 19:07:44 -0400

    change

--- assignments/assignment02/js/magie.js
+++ assignments/assignment02/js/magie.js
@@ -1,9 +1,10 @@
 // la durée des animations utilisant scrollmagic
 var scrollTemps = 1000;
 
-
 var grandScroll = "-=60";
 var grandScrollPositif = "+=60";
+
+var trigScroll = 1;
 
 
 
@@ -11,17 +12,19 @@ var grandScrollPositif = "+=60";
 // Fonction débute lorsque le site est téléchargé
 $(document).ready(function() {
 
-
     
 
 
 // Scrollmagic    
 
+    
     var controller = new ScrollMagic.Controller({globalSceneOptions: {duration: scrollTemps}});
+    
     
     
 
 // Scrollmagic animation 1
+    
     
     var anim1 = new ScrollMagic.Scene({
         triggerElement: "#anim1",
@@ -31,7 +34,7 @@ $(document).ready(function() {
         left: grandScroll
     });
     
-    anim1.triggerHook(0.5);
+    anim1.triggerHook(trigScroll);
     anim1.addTo(controller);
     
     
@@ -42,27 +45,27 @@ $(document).ready(function() {
         backgroundColor: "black"
     });
 
-    anim1b.triggerHook(0.5);
+    anim1b.triggerHook(trigScroll);
     anim1b.addTo(controller);
+    
     
     
 
 // Scrollmagic animation 2
     
+    
     var anim2 = new ScrollMagic.Scene({
         triggerElement: "#anim2",
     });
 
-    anim2.setTween("#c2,#c1", {
+    anim2.setTween("#c1,#c2", {
         top: grandScroll
     });
 
-    anim2.triggerHook(0.5);
+    anim2.triggerHook(trigScroll);
     anim2.addTo(controller);
     
-    
-    
-    
+        
     var anim2b = new ScrollMagic.Scene({
         triggerElement: "#anim2",
     });
@@ -71,23 +74,24 @@ $(document).ready(function() {
         left: grandScroll
     });
 
-    
-    anim2b.triggerHook(0.5);
+    anim2b.triggerHook(trigScroll);
     anim2b.addTo(controller);
     
     
 
+    
 // Scrollmagic animation 3
+    
     
     var anim3 = new ScrollMagic.Scene({
         triggerElement: "#anim3",
     });
 
-    anim3.setTween("#c1,#c2,#b1", {
+    anim3.setTween("#b1,#c1,#c2", {
         top: grandScroll
     });
 
-    anim3.triggerHook(0.5);
+    anim3.triggerHook(trigScroll);
     anim3.addTo(controller);
     
     
@@ -96,26 +100,28 @@ $(document).ready(function() {
         duration: scrollTemps
     });
 
-    anim3b.setTween("#c5,#c4,#c3", {
+    anim3b.setTween("#c3,#c4,#c5", {
         top: grandScroll
     });
 
-    anim3b.triggerHook(0.5);
+    anim3b.triggerHook(trigScroll);
     anim3b.addTo(controller);
     
     
 
+    
 // Scrollmagic animation 4
+    
     
     var anim4 = new ScrollMagic.Scene({
         triggerElement: "#anim4",
     });
 
-    anim4.setTween("#c1,#c2,#b1,#a1", {
+    anim4.setTween("#a1,#b1,#c1,#c2", {
         left: grandScrollPositif
     });
 
-    anim4.triggerHook(0.5);
+    anim4.triggerHook(trigScroll);
 
     anim4.addTo(controller);
     
@@ -124,29 +130,35 @@ $(document).ready(function() {
         triggerElement: "#anim4",
     });
 
-    anim4b.setTween("#c5,#c4,#c3,#b3", {
+    anim4b.setTween("#b3,#c3,#c4,#c5", {
         left: grandScroll
     });
 
-    anim4b.triggerHook(0.5);
+    anim4b.triggerHook(trigScroll);
     anim4b.addTo(controller);
     
     
+    
+    
 // Scrollmagic animation 5
+    
     
     var anim5 = new ScrollMagic.Scene({
         triggerElement: "#anim5",
     });
 
-    anim5.setTween("#c5,#c4,#c3,#b3,#b2", {
+    anim5.setTween("#c3,#c4,#c5,#b2,#b3", {
         top: grandScroll
     });
 
-    anim5.triggerHook(0.5);
+    anim5.triggerHook(trigScroll);
     anim5.addTo(controller);
     
     
+    
+    
 // Scrollmagic animation 6
+    
     
     var anim6 = new ScrollMagic.Scene({
         triggerElement: "#anim6",
@@ -155,7 +167,7 @@ $(document).ready(function() {
         left: grandScrollPositif
     });
 
-    anim6.triggerHook(0.5);
+    anim6.triggerHook(trigScroll);
     anim6.addTo(controller);
     
     
@@ -167,7 +179,7 @@ $(document).ready(function() {
         top: grandScrollPositif
     });
 
-    anim6b.triggerHook(0.5);
+    anim6b.triggerHook(trigScroll);
     anim6b.addTo(controller);
     
     
@@ -179,12 +191,14 @@ $(document).ready(function() {
         left: grandScroll
     });
 
-    anim6c.triggerHook(0.5);
+    anim6c.triggerHook(trigScroll);
     anim6c.addTo(controller);
     
     
     
+    
 // Scrollmagic animation 7
+    
     
     var anim7 = new ScrollMagic.Scene({
         triggerElement: "#anim7"
@@ -194,7 +208,7 @@ $(document).ready(function() {
         left: grandScrollPositif
     });
 
-    anim7.triggerHook(0.5);
+    anim7.triggerHook(trigScroll);
     anim7.addTo(controller);
     
     
@@ -205,18 +219,18 @@ $(document).ready(function() {
         top: grandScrollPositif
     });
 
-    anim7b.triggerHook(0.5);
+    anim7b.triggerHook(trigScroll);
     anim7b.addTo(controller);
     
     
    var anim7c = new ScrollMagic.Scene({
         triggerElement: "#anim7"
     });
-    anim7c.setTween("#d3,#d2", {
+    anim7c.setTween("#d2,#d3", {
         top: grandScrollPositif
     });
 
-    anim7c.triggerHook(0.5);
+    anim7c.triggerHook(trigScroll);
     anim7c.addTo(controller);
     
     
@@ -231,12 +245,10 @@ $(document).ready(function() {
         top: grandScrollPositif
     });
 
-    anim8.triggerHook(0.5);
+    anim8.triggerHook(trigScroll);
     anim8.addTo(controller);
     
     
-
-
     var anim8b = new ScrollMagic.Scene({
         triggerElement: "#anim8",
     });
@@ -244,19 +256,240 @@ $(document).ready(function() {
         left: grandScrollPositif
     });
 
-    anim8b.triggerHook(0.5);
+    anim8b.triggerHook(trigScroll);
     anim8b.addTo(controller);
     
     
     var anim8c = new ScrollMagic.Scene({
         triggerElement: "#anim8"
     });
-    anim8c.setTween("#d3,#d2,#e2", {
+    anim8c.setTween("#d2,#d3,#e2", {
         top: grandScrollPositif
     });
 
-    anim8c.triggerHook(0.5);
+    anim8c.triggerHook(trigScroll);
     anim8c.addTo(controller);
+    
+    
+    var anim8d = new ScrollMagic.Scene({
+        triggerElement: "#anim8"
+    });
+    anim8d.setTween("#d4", {
+        left: grandScrollPositif
+    });
+
+    anim8d.triggerHook(trigScroll);
+    anim8d.addTo(controller);   
+
+
+    
+    
+// Scrollmagic animation 9
+    
+    
+    var anim9 = new ScrollMagic.Scene({
+        triggerElement: "#anim9"
+    });
+    
+    anim9.setTween("#a1,#a2,#a3,#a4,#b1,#b2,#b3,#b4,#c1,#c2,#c3,#c4,#c5,#d1,#d2,#d3,#e1,#e2,#e3,#f1,#f2", {
+        left: grandScrollPositif
+    });
+
+    anim9.triggerHook(trigScroll);
+    anim9.addTo(controller);
+    
+    
+    var anim9b = new ScrollMagic.Scene({
+        triggerElement: "#anim9"
+    });
+    
+    anim9b.setTween("#d4,#d5", {
+        top: grandScrollPositif
+    });
+
+    anim9b.triggerHook(trigScroll);
+    anim9b.addTo(controller);
+    
+    
+    
+    
+// Scrollmagic animation 10
+    
+    
+    var anim10 = new ScrollMagic.Scene({
+        triggerElement: "#anim10"
+    });
+    
+    anim10.setTween("#a1,#a2,#a3,#a4,#b1,#b2,#b3,#b4,#b5,#c1,#c2,#c3,#c4,#c5", {
+        top: grandScroll
+    });
+
+    anim10.triggerHook(trigScroll);
+    anim10.addTo(controller);
+    
+    
+    var anim10b = new ScrollMagic.Scene({
+        triggerElement: "#anim10"
+    });
+    
+    anim10b.setTween("#e3,#e4", {
+        top: grandScrollPositif
+    });
+
+    anim10b.triggerHook(trigScroll);
+    anim10b.addTo(controller);
+    
+    
+    var anim10c = new ScrollMagic.Scene({
+        triggerElement: "#anim10"
+    });
+    
+    anim10c.setTween("#d1,#d2,#d3,#e1,#e2,#f1,#f2,#f3", {
+        left: grandScrollPositif
+    });
+
+    anim10c.triggerHook(trigScroll);
+    anim10c.addTo(controller);
+    
+
+
+    
+// Scrollmagic animation 11
+    
+    
+    var anim11 = new ScrollMagic.Scene({
+        triggerElement: "#anim11"
+    });
+    
+    anim11.setTween("#d4,#d5,#e5", {
+        top: grandScrollPositif
+    });
+
+    anim11.triggerHook(trigScroll);
+    anim11.addTo(controller);
+    
+    
+    var anim11b = new ScrollMagic.Scene({
+        triggerElement: "#anim11"
+    });
+    
+    anim11b.setTween("#d1,#d2,#d3,#e1,#e2,#e3,#e4,#f1,#f2,#f3,#f4", {
+        left: grandScrollPositif
+    });
+
+    anim11b.triggerHook(trigScroll);
+    anim11b.addTo(controller);
+    
+
+    var anim11c = new ScrollMagic.Scene({
+        triggerElement: "#anim11"
+    });
+    
+    anim11c.setTween("#a1,#a2,#a3,#a4,#a5,#b1,#b2,#b3,#b4,#b5,#c1,#c2,#c3,#c4,#c5", {
+        left: grandScrollPositif
+    });
+
+    anim11c.triggerHook(trigScroll);
+    anim11c.addTo(controller);
+    
+
+    
+    
+// Scrollmagic animation 12
+    
+    
+    var anim12 = new ScrollMagic.Scene({
+        triggerElement: "#anim12"
+    });
+    
+    anim12.setTween("#d1,#d2,#d3,#d4,#d5,#e1,#e2,#e3,#e4,#e5,#f1,#f2,#f3,#f4,#f5", {
+        left: grandScrollPositif
+    });
+
+    anim12.triggerHook(trigScroll);
+    anim12.addTo(controller);
+    
+    
+    var anim12b = new ScrollMagic.Scene({
+        triggerElement: "#anim12"
+    });
+    
+    anim12b.setTween("#a1,#a2,#a3,#a4,#a5,#a6,#b1,#b2,#b3,#b4,#b5,#c1,#c2,#c3,#c4,#c5", {
+        top: grandScrollPositif
+    });
+
+    anim12b.triggerHook(trigScroll);
+    anim12b.addTo(controller);
+    
+    
+    
+    
+// Scrollmagic animation 13
+    
+    
+    var anim13 = new ScrollMagic.Scene({
+        triggerElement: "#anim13"
+    });
+    
+    anim13.setTween("#d1,#d2,#d3,#d4,#d5,#e1,#e2,#e3,#e4,#e5,#f1,#f2,#f3,#f4,#f5,#f6", {
+        top: grandScroll
+    });
+
+    anim13.triggerHook(trigScroll);
+    anim13.addTo(controller);
+    
+    
+    var anim13b = new ScrollMagic.Scene({
+        triggerElement: "#anim13"
+    });
+    
+    anim13b.setTween("#a1,#a2,#a3,#a4,#a5,#a6,#b1,#b2,#b3,#b4,#b5,#b6,#c1,#c2,#c3,#c4,#c5", {
+        top: grandScrollPositif
+    });
+
+    anim13b.triggerHook(trigScroll);
+    anim13b.addTo(controller);
+    
+    
+    
+    
+// Scrollmagic animation 14
+    
+    
+    var anim14 = new ScrollMagic.Scene({
+        triggerElement: "#anim14"
+    });
+    
+    anim14.setTween("#d1,#d2,#d3,#d4,#d5,#e1,#e2,#e3,#e4,#e5,#e6,#f1,#f2,#f3,#f4,#f5,#f6", {
+        top: grandScroll
+    });
+
+    anim14.triggerHook(trigScroll);
+    anim14.addTo(controller);
+    
+    
+    var anim14b = new ScrollMagic.Scene({
+        triggerElement: "#anim14"
+    });
+    
+    anim14b.setTween("#a1,#a2,#a3,#a4,#a5,#a6,#b1,#b2,#b3,#b4,#b5,#b6,#c1,#c2,#c3,#c4,#c5,#c6", {
+        top: grandScrollPositif
+    });
+
+    anim14b.triggerHook(trigScroll);
+    anim14b.addTo(controller);
+    
+    
+    var anim14c = new ScrollMagic.Scene({
+        triggerElement: "#anim14"
+    });
+    
+    anim14c.setTween("#a1,#a2,#a3,#a4,#a5,#a6,#b1,#b2,#b3,#b4,#b5,#b6,#c1,#c2,#c3,#c4,#c5,#c6,#d1,#d2,#d3,#d4,#d5,#d6,#e1,#e2,#e3,#e4,#e5,#e6,#f1,#f2,#f3,#f4,#f5,#f6", {
+        backgroundColor: "blue"
+    });
+
+    anim14c.triggerHook(trigScroll);
+    anim14c.addTo(controller);
     
     
     
--- assignments/assignment02/js/script.js
+++ assignments/assignment02/js/script.js
@@ -1,19 +1,22 @@
-// la durée des animations
+// La durée des animations
 var temps = 1000;
 
-// la durée des animations plus lente
+// La durée des animations plus lente
 var tempsLent = 1000;
 
+// La dimension des carrés
 var grand = "60px";
 
-
+// La distance du défilement du texte
 var texteDrop = "+=55";
 
-
+// Les couleurs principales
 var coul = "white";
 var coulNoir = "black";
+var coulFade = "0.2";
 
 
+// Les rangés de carrés
 var serieA = '#a1,#a2,#a3,#a4,#a5,#a6';
 var serieB = '#b1,#b2,#b3,#b4,#b5,#b6';
 var serieC = '#c1,#c2,#c3,#c4,#c5,#c6';
@@ -21,8 +24,14 @@ var serieD = '#d1,#d2,#d3,#d4,#d5,#d6';
 var serieE = '#e1,#e2,#e3,#e4,#e5,#e6';
 var serieF = '#f1,#f2,#f3,#f4,#f5,#f6';
 
+// Les carrés formant un point d'interrogation
 var serieInterrogation = '#a3,#a4,#b2,#b5,#c5,#d4,#f4';
 var serieInterrogationVide = '#a1,#a2,#a5,#a6,#b1,#b3,#b4,#b6,#c1,#c2,#c3,#c4,#c6,#d1,#d2,#d3,#d5,#d6,#e1,#e2,#e3,#e4,#e5,#e6,#f1,#f2,#f3,#f5,#f6';
+
+// Tous les carrés
+var serieZ = '#a1,#a2,#a3,#a4,#a5,#a6,#b1,#b2,#b3,#b4,#b5,#b6,#c1,#c2,#c3,#c4,#c5,#c6,#d1,#d2,#d3,#d4,#d5,#d6,#e1,#e2,#e3,#e4,#e5,#e6,#f1,#f2,#f3,#f4,#f5,#f6';
+    
+
 
 
 
@@ -34,6 +43,9 @@ var serieInterrogationVide = '#a1,#a2,#a5,#a6,#b1,#b3,#b4,#b6,#c1,#c2,#c3,#c4,#c
 // Fonction débute lorsque le site est téléchargé
 $(document).ready(function() {
 
+
+$(serieZ).addClass("abso");   
+    
 $( "#texte1" ).animate({ top: texteDrop }, tempsLent);
     
 $( serieInterrogation ).animate({ height:grand }, tempsLent);
@@ -45,7 +57,7 @@ $(serieInterrogation).click(function () {
     if  (
             ($(serieInterrogationVide).height() == 0)
         ){
-            $( "#texte1" ).animate({ top: texteDrop, opacity: 0.25 }, temps);    
+            $( "#texte1" ).animate({ top: texteDrop, opacity: coulFade }, temps);    
             $( "#texte2" ).animate({ top: texteDrop }, temps);    
             $('#a3').animate({ height:"0px", backgroundColor:"#f8f4ed" }, temps);
             $('#a4').animate({ height:"0px", backgroundColor:"#fefbf8" }, temps);
@@ -66,12 +78,10 @@ $(serieInterrogation).click(function () {
                                 $('#f4').animate({ height:"0px", backgroundColor:"#51332d" }, temps);
                                 $('#f1,#f2,#f3,#f5').animate({ height:grand }, temps);         
                                 $('#f6').animate({ height:grand }, temps, function() {
-                                    $("body").removeClass("scrollhide");
                                     $("#full").removeClass("fix");
                                     $("#full").addClass("relat", function() {
                                         $(serieInterrogation).animate({ height:grand }, temps);
-                                        $( "#texte1" ).animate({ top: texteDrop, opacity: 0.25 }, temps);
-                                        $( "#texte2" ).animate({ top: texteDrop, opacity: 0.25 }, temps);
+                                        $( "#texte1,#texte2" ).animate({ top: texteDrop, opacity: coulFade }, temps);
                                         $( "#texte3" ).animate({ top: texteDrop }, temps, function() {
                                             $("#image1").animate({ height:"360px" }, temps);
                                         });
@@ -86,6 +96,76 @@ $(serieInterrogation).click(function () {
 });
 
     
+// Le carré bleu se déplace vers le bas
+$("#f6").click(function () {   
+    if  (
+            ($(serieZ).position().top == 180)&&
+            ($(serieZ).position().left == 300)
+        ){
+            $(serieZ).animate({ backgroundColor:"white", top: 300 }, temps);
+            $( "#texte4" ).animate({ top: texteDrop }, temps);
+            $( "#texte1,#texte2,#texte3" ).animate({ top: texteDrop, opacity: coulFade }, temps);
+            $("#image2").animate({ height:"360px" }, temps);
+            $("#image1").animate({ height:"0px" }, temps);
+        }
+});
+    
+    
+// Le carré blanc active le positionnement des autres carrés
+$("#f6").click(function () {   
+    if  (
+            ($(serieZ).position().top == 300)&&
+            ($(serieZ).position().left == 300)
+        ){
+            $( "#texte5" ).animate({ top: texteDrop }, temps);
+            $( "#texte1,#texte2,#texte3,#texte4" ).animate({ top: texteDrop, opacity: coulFade }, temps);    
+            $("body").animate({ backgroundColor: coul, color: coulNoir},temps);
+            $("#grand").animate({ backgroundColor: coul},temps);
+            $("#image2").animate({ width:"0px" }, temps);
+        
+            $("#a1").animate({ left: "-80px", top: "60px", backgroundColor:"#9e978f" }, temps);
+            $("#a2").animate({ left: "-200px", top: "200px", backgroundColor:"#1f1611" }, temps);
+            $("#a3").animate({ left: "-60px", top: "60px", backgroundColor:"#edceaf" }, temps);
+            $("#a4").animate({ left: "-70px", top: "110px", backgroundColor:"#efe5e4" }, temps);
+            $("#a5").animate({ left: "-90px", top: "40px", backgroundColor:"#ece6e6" }, temps);
+            $("#a6").animate({ left: "-150px", top: "290px", backgroundColor:"#ececca" }, temps);
+        
+            $("#b1").animate({ left: "-90px", top: "100px", backgroundColor:"#2f221a" }, temps);
+            $("#b2").animate({ left: "-70px", top: "0px", backgroundColor:"#130e0b" }, temps);
+            $("#b3").animate({ left: "-120px", top: "20px", backgroundColor:"#e7ecd5" }, temps);
+            $("#b4").animate({ left: "-80px", top: "140px", backgroundColor:"#eae4e4" }, temps);
+            $("#b5").animate({ left: "-100px", top: "300px", backgroundColor:"#ebebe3" }, temps);
+            $("#b6").animate({ left: "-60px", top: "200px", backgroundColor:"#eccfa3" }, temps);
+        
+            $("#c1").animate({ left: "-60px", top: "200px", backgroundColor:"#a89ea6" }, temps);
+            $("#c2").animate({ left: "-60px", top: "60px", backgroundColor:"#563830" }, temps);
+            $("#c3").animate({ left: "-150px", top: "160px", backgroundColor:"#ebd1b8" }, temps);
+            $("#c4").animate({ left: "-100px", top: "100px", backgroundColor:"#ecb4b3" }, temps);
+            $("#c5").animate({ left: "-70px", top: "0px", backgroundColor:"#ecc0b5" }, temps);
+            $("#c6").animate({ left: "-80px", top: "50px", backgroundColor:"#e99068" }, temps);
+        
+            $("#d1").animate({ left: "-120px", top: "80px", backgroundColor:"#aa9aa7" }, temps);
+            $("#d2").animate({ left: "-210px", top: "30px", backgroundColor:"#ac8b92" }, temps);
+            $("#d3").animate({ left: "-60px", top: "160px", backgroundColor:"#e58a91" }, temps);
+            $("#d4").animate({ left: "-130px", top: "260px", backgroundColor:"#eb898a" }, temps);
+            $("#d5").animate({ left: "-160px", top: "120px", backgroundColor:"#d57867" }, temps);
+            $("#d6").animate({ left: "-90px", top: "40px", backgroundColor:"#904f39" }, temps);
+        
+            $("#e1").animate({ left: "-180px", top: "150px", backgroundColor:"#6e3228" }, temps);
+            $("#e2").animate({ left: "-160px", top: "180px", backgroundColor:"#6b3326" }, temps);
+            $("#e3").animate({ left: "-80px", top: "80px", backgroundColor:"#994c38" }, temps);
+            $("#e4").animate({ left: "-60px", top: "240px", backgroundColor:"#603223" }, temps);
+            $("#e5").animate({ left: "-220px", top: "80px", backgroundColor:"#312620" }, temps);
+            $("#e6").animate({ left: "-130px", top: "130px", backgroundColor:"#5b5348" }, temps);
+
+            $("#f1").animate({ left: "-90px", top: "150px", backgroundColor:"#511a17" }, temps);
+            $("#f2").animate({ left: "-60px", top: "70px", backgroundColor:"#6b3326" }, temps);
+            $("#f3").animate({ left: "-100px", top: "110px", backgroundColor:"#994c38" }, temps);
+            $("#f4").animate({ left: "-150px", top: "190px", backgroundColor:"#603223" }, temps);
+            $("#f5").animate({ left: "-120px", top: "140px", backgroundColor:"#312620" }, temps);
+            $("#f6").animate({ left: "-210px", top: "220px", backgroundColor:"#5b5348" }, temps);
+        }
+});
     
     
 });
